59bf2ad1d7eec85b0488cddf59e83f66b3b942f5
Namyun Kim <skadvs@gmail.com>
2014-07-26 21:21:32 +0900

    Change git fetcher to Github Webhook

---
+++ GitEventChecker/server.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var gith = require('gith').create(9001);
+
+gith({
+	repo: 'proxyer/LiveUpdater',
+	branch: 'develop'
+}).on('all', function (payload) {
+	console.log(payload);
+});
