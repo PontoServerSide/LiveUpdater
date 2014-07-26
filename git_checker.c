1eaa21eac8bd5f63e22a20a30da2d3b75be5266a
Namyun Kim <skadvs@gmail.com>
2014-07-26 16:15:40 +0900

    Add polling feature

--- git_checker.c
+++ git_checker.c
@@ -11,13 +11,46 @@
 #include <string.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <signal.h>
+#include <sys/time.h>
 
-#define LOGGER
-
-#define NO_ERROR    0
+git_repository *repo = NULL;
+git_remote *remote = NULL;
+char buf[GIT_OID_HEXSZ + 1];
+int error;
 
 void interval_fetch_handler(int signum) {
 
+    static int timer_count = 0;
+    
+
+    const git_remote_head **remote_heads = NULL;
+    
+    size_t count = 0;
+    size_t i = 0;
+    if ((error = git_remote_ls(&remote_heads, &count, remote))<0) {
+        return;
+    }
+
+    for (; i<count; ++i) {
+        const git_remote_head *head = remote_heads[i];
+        if(strstr(head->name,"HEAD")!=NULL) {
+            git_oid oid;
+            if ((error = git_reference_name_to_id(&oid, repo, "HEAD"))<0) {
+                return;
+            }
+
+            git_commit *commit;
+            if ((error = git_commit_lookup(&commit, repo, &oid))<0) {
+                return;
+            }
+
+            const git_oid *commit_id = git_commit_id(commit);   // Get commit id
+            git_oid_tostr(buf, sizeof(buf), commit_id);         // Convert heximal commit id to string
+
+            fprintf(stdout, "Check ARLAM %d %s\n", ++timer_count, buf);
+        }
+    }
 }
 
 void git_error_handler(int error) {
@@ -28,44 +61,7 @@ void git_error_handler(int error) {
     }
 }
 
-int get_latest_commit(git_repository *repo, git_remote *remote, char *buf) {
-    const git_remote_head **remote_heads = NULL;
-    
-    size_t count = 0;
-    size_t i = 0;
-    int error;
-    if ((error = git_remote_ls(&remote_heads, &count, remote))<0) {
-        return error;
-    }
-
-    for (; i<count; ++i) {
-        const git_remote_head *head = remote_heads[i];
-        if(strstr(head->name,"HEAD")!=NULL) {
-            git_oid oid;
-            if ((error = git_reference_name_to_id(&oid, repo, "HEAD"))<0) {
-                return error;
-            }
-
-            git_commit *commit;
-            if ((error = git_commit_lookup(&commit, repo, &oid))<0) {
-                return error;
-            }
-
-            const git_oid *commit_id = git_commit_id(commit);   // Get commit id
-            git_oid_tostr(buf, sizeof(buf), commit_id);         // Convert heximal commit id to string
-
-            return 0;
-        }
-    }
-
-    return 0;
-}
-
 int main(int argc, char** argv) {
-    git_repository *repo = NULL;
-    git_remote *remote = NULL;
-    int error;
-
     struct sigaction sa;
     struct itimerval timer;
 
@@ -74,17 +70,6 @@ int main(int argc, char** argv) {
         return -1;
     }
 
-    // initialize timer
-    memset(&sa,0,sizeof(sa));
-    sa.sa_handler = &interval_fetch_handler;
-    sigaction(SIGVTALAM,&sa,NULL);
-
-    // set timer interval
-    timer.it_value.tv_sec = 10;
-    timer.it_interval.tv_sec = 10;
-
-    setitimer(ITIMER_VIRTUAL,&timer,NULL);
-
     git_error_handler(git_clone(&repo, argv[2], argv[1], NULL));    
 
     git_error_handler(git_remote_load(&remote, repo, "origin"));
@@ -92,8 +77,21 @@ int main(int argc, char** argv) {
     git_error_handler(git_remote_connect(remote, GIT_DIRECTION_FETCH));
     int connected = git_remote_connected(remote);
 
-    char buf[GIT_OID_HEXSZ + 1];
-    git_error_handler(get_latest_commit(repo, remote, buf));
+    // initialize timer
+    memset(&sa,0,sizeof(sa));
+    sa.sa_handler = &interval_fetch_handler;
+    sigaction(SIGVTALRM,&sa,NULL);
+
+    // set timer interval
+    timer.it_value.tv_sec = 1;
+    timer.it_value.tv_usec = 0;
+
+    timer.it_interval.tv_sec = 1;
+    timer.it_interval.tv_usec = 0;
+
+    setitimer(ITIMER_VIRTUAL,&timer,NULL);
+
+    while(1);
 
     printf("Last commit: %s\n", buf);
 
