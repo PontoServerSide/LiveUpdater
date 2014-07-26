92a4d64481d3c50d96aeecc04117fc6a43dc705f
Namyun Kim <skadvs@gmail.com>
2014-07-26 22:29:40 +0900

    Add winston logger

--- GitEventChecker/server.js
+++ GitEventChecker/server.js
@@ -1,7 +1,10 @@
 'use strict';
 
 var gith = require('gith').create(9001);
+var winston = require('winston');
 
-gith().on('all', function (payload) {
-	console.log(payload);
+gith({
+	repo: 'proxyer/LiveUpdater'
+}).on('all', function (payload) {
+	winston.debug(payload);
 });
