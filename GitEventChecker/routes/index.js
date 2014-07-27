be5f0c8170c60227311157a15ffd80ce03f1ab55
Namyun Kim <skadvs@gmail.com>
2014-07-27 12:20:21 +0900

    Remove json parser

--- GitEventChecker/routes/index.js
+++ GitEventChecker/routes/index.js
@@ -3,8 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.post('/', function(req, res) {
-	var param = JSON.parse(req.body.payload);
-	console.log(param);
+	//var param = JSON.parse(req.body.payload);
+	console.log(req.body.payload);
 
 	res.send('complete');
 });
