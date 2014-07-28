b0a7212e337b7dfe94f05ca3cbbb89b66caac1ef
Namyun Kim <skadvs@gmail.com>
2014-07-28 21:36:44 +0900

    Write the code to read haproxy socket

--- GitEventChecker/routes/index.js
+++ GitEventChecker/routes/index.js
@@ -1,10 +1,30 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express'),
+	router = express.Router(),
+	net = require('net'),
+	fs = require('fs'),
+	socketPath = '/tmp/haproxy';
+
 
 /* GET home page. */
 router.post('/', function(req, res) {
-	//var param = JSON.parse(req.body.payload);
+	var param = JSON.parse(req.body);
 	console.log(req.body);
+
+	fs.stat(socketPath, function (err) {
+		if (!err) {
+			fs.unlinkSync(socketPath);
+		}
+
+		var unixServer = net.createServer(function (sock) {
+			sock.write('show health');
+
+			sock.on('data', function (data) {
+				winston.debug('DATA: '+data);
+				sock.destroy();
+			});
+
+		}).listen(socketPath);
+	});
 
 	res.send('complete');
 });
